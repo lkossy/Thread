836be04a3b26d12b13b760446f3a785dbecff1dc
水越雅紀 <@DESKTOP-PPCICF2>
2017-11-28 11:41:52 +0900

    Thread_challenge

---
+++ src/thread/Thread_main.java
@@ -0,0 +1,32 @@
+package thread;
+
+public class Thread_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Thread_run [] thr1 = new Thread_run[3];
+		Thread_runnable [] thr2 = new Thread_runnable[3]; 
+		
+		for(int i = 0; i < 1; i++) {
+			thr2[i]  = new Thread_runnable();
+			thr2[i].run();
+			thr1[i]  = new Thread_run();
+			thr1[i].start();
+		
+	
+		try {
+			thr1[i].join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}
+		for(int i = 0; i < 10; i++) {
+			System.out.println("main‚©‚ço—Í :"+i);
+		}
+	}
+	
+}
+
+	
+
